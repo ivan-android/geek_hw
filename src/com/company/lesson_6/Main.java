e6130502831c6da112144139c860d359779448f1
Ivan <ipopov.atc@gmail.com>
2021-04-06 10:18:13 +0300

    Реализация ДЗ№6

---
+++ src/com/company/lesson_6/Main.java
@@ -0,0 +1,29 @@
+package com.company.lesson_6;
+
+
+import com.company.lesson_5.Employee;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Cat cat = new Cat(200);
+        Cat cat1 = new Cat(200);
+        Cat cat2 = new Cat(200);
+        Cat cat3 = new Cat(200);
+
+        Dog dog = new Dog(500, 10);
+        Dog dog2 = new Dog(500, 10);
+        Dog dog3 = new Dog(500, 10);
+        Animal.getCount();
+
+
+
+
+    }
+
+
+}
+
+
